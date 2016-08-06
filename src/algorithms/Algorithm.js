650893de1385935a79b73556007bbe1843441d63
Christian Smith <smith@anvil.io>
2016-08-06 15:28:26 -0700

    refactor(Algorithm): constructor takes object argument

--- src/algorithms/Algorithm.js
+++ src/algorithms/Algorithm.js
@@ -1,10 +1,27 @@
 /**
- * Algorithm dictionary
+ * Algorithm
  */
 class Algorithm {
-  constructor (name, op) {
-    this.name = name
-    this.op = op
+
+  /**
+   * constructor
+   *
+   * @description
+   * The Algorithm object is a dictionary object [WebIDL] which is used
+   * to specify an algorithm and any additional parameters required to
+   * fully specify the desired operation.
+   *
+   * @param {string|Object} algorithm
+   */
+  constructor (algorithm) {
+    if (typeof algorithm === 'string') {
+      this.name = algorithm
+    } else {
+      Object.assign(this, algorithm)
+      if (typeof this.name !== 'string') {
+        throw new Error('Algorithm name must be a string')
+      }
+    }
   }
 }
 
