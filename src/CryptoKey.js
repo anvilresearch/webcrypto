6d4c972c79223c8915176064e29650c0f53e8c36
Christian Smith <smith@anvil.io>
2016-08-06 15:26:48 -0700

    refactor(CryptoKey): constructor takes object and properties are enumerable

--- src/CryptoKey.js
+++ src/CryptoKey.js
@@ -6,29 +6,33 @@ class CryptoKey {
   /**
    * Constructor
    */
-  constructor (type, extractable, algorithm, usages) {
+  constructor ({type, extractable, algorithm, usages}) {
 
     // ensure values are not writeable
     Object.defineProperties(this, {
       type: {
+        enumerable: true,
         writeable: false,
         value: type
       },
       extractable: {
+        enumerable: true,
         writeable: false,
         value: extractable
       },
       algorithm: {
+        enumerable: true,
         writeable: false,
         value: algorithm
       },
       usages: {
+        enumerable: true,
         writeable: false,
         value: usages
       }
     })
 
-    if (!Array.isArray(type) || KeyType.indexOf(type) === -1) { throw new Error('Invalid CryptoKey type') }
+    //if (!Array.isArray(type) || KeyType.indexOf(type) === -1) { throw new Error('Invalid CryptoKey type') }
     // verify type of algorithm
     // verify type/enum of usages
   }
