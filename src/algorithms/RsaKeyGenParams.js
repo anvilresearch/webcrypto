9fe8e5698b7859bb1c00ed9beee0ffc110a12c06
Christian Smith <smith@anvil.io>
2016-08-06 15:24:52 -0700

    feat(RsaKeyGenParams): class definition

---
+++ src/algorithms/RsaKeyGenParams.js
@@ -0,0 +1,39 @@
+/**
+ * Local dependencies
+ */
+const Algorithm = require('./Algorithm')
+
+/**
+ * RsaKeyGenParams
+ */
+class RsaKeyGenParams extends Algorithm {
+
+  /**
+   * Constructor
+   *
+   * @param {number} modulusLength
+   * @param {BigInteger} publicExponent
+   */
+  constructor (algorithm) {
+    super(algorithm)
+
+    // validate modulusLength
+    if (typeof this.modulusLength !== 'number') {
+      throw new Error()
+    }
+
+    if (this.modulusLength < 1024) {
+      throw new Error()
+    }
+
+    // validate publicExponent
+    if (!(this.publicExponent instanceof BigInteger)) {
+      throw new Error()
+    }
+  }
+}
+
+/**
+ * Export
+ */
+module.exports = RsaKeyGenParams
