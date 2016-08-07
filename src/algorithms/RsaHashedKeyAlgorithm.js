445ca6b1cc037b7528eb9f5b66fcf7960fc2aa80
Christian Smith <smith@anvil.io>
2016-08-07 15:39:01 -0700

    wip(RsaHashedKeyAlgorithm): sign method

--- src/algorithms/RsaHashedKeyAlgorithm.js
+++ src/algorithms/RsaHashedKeyAlgorithm.js
@@ -2,6 +2,7 @@
  * Package dependencies
  */
 const RSA = require('node-rsa')
+const crypto = require('crypto')
 
 /**
  * Local dependencies
@@ -55,14 +56,29 @@ class RsaHashedKeyAlgorithm extends RsaKeyAlgorithm {
    * sign
    *
    * @description
+   * Create an RSA digital signature
    *
-   * @param {}
+   * @param {CryptoKey} key
+   * @param {BufferSource} data
    *
-   * @returns {}
+   * @returns {string}
    */
-  sign () {
-    // TODO
-    throw new NotSupportedError()
+  sign (key, data) {
+    if (key.type !== 'private') {
+      throw new InvalidAccessError()
+    }
+
+    try {
+      let pem = key.key
+      let signer = crypto.createSign('RSA-SHA256')
+      signer.update(data.toString())
+      let signature = signer.sign(pem)
+      // TODO
+      // return an ArrayBuffer representation of the signature
+      return signature
+    } catch (error) {
+      throw error //new OperationError()
+    }
   }
 
   /**
@@ -120,23 +136,24 @@ class RsaHashedKeyAlgorithm extends RsaKeyAlgorithm {
    */
   generateKey (params, extractable, usages) {
     // validate usages
-    // TODO where does usages come from? arg? this?
     usages.forEach(usage => {
       if (usage !== 'sign' && usage !== 'verify') {
         throw new SyntaxError()
       }
     })
 
-    // Generate and RSA keypair
+    let keypair
+
+    // Generate RSA keypair
     try {
       // TODO
       // what is this bit option, where do we get the value from in this
       // api?
       let key = new RSA({b:512})
       let {modulusLength,publicExponent} = params
-      let keypair = key.generateKeyPair(modulusLength, publicExponent)
+      keypair = key.generateKeyPair(modulusLength, publicExponent)
 
-      console.log(keypair.exportKey('public'), keypair.exportKey('private'))
+      //console.log(keypair.exportKey('public'), keypair.exportKey('private'))
       // TODO
       // - fallback on system OpenSSL + child_process
       // - how do we bind the results to the generated CryptoKey objects?
@@ -152,7 +169,8 @@ class RsaHashedKeyAlgorithm extends RsaKeyAlgorithm {
       type: 'public',
       algorithm,
       extractable: true,
-      usages: ['verify']
+      usages: ['verify'],
+      key: keypair.exportKey('public')
     })
 
     // instantiate privateKey
@@ -162,13 +180,12 @@ class RsaHashedKeyAlgorithm extends RsaKeyAlgorithm {
       // TODO is there a typo in the spec?
       // it says "extractable" instead of "false"
       extractable: false,
-      usages: ['sign']
+      usages: ['sign'],
+      key: keypair.exportKey('private')
     })
 
     // return a new keypair
     return new CryptoKeyPair({publicKey,privateKey})
-    // TODO
-    throw new NotSupportedError()
   }
 
   /**
