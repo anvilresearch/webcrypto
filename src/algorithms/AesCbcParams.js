a5aaf0133430016760f1e94e2d9cc990ddfd3dff
Christian Smith <smith@anvil.io>
2016-08-09 21:23:04 -0700

    wip(AesCbcParams): class definition

---
+++ src/algorithms/AesCbcParams.js
@@ -0,0 +1,20 @@
+/**
+ * Local dependencies
+ */
+const Algorithm = require('./Algorithm')
+
+/**
+ * AesCbcParams
+ */
+class AesCbcParams extends Algorithm {
+
+  /**
+   * Constructor
+   */
+  constructor () {}
+}
+
+/**
+ * Export
+ */
+module.exports = AesCbcParams
