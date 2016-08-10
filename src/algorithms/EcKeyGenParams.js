79bb08f7f3b0ecba40463fce9ff4cc95035413ab
Christian Smith <smith@anvil.io>
2016-08-09 21:11:14 -0700

    wip(EcKeyGenParams): class definition

---
+++ src/algorithms/EcKeyGenParams.js
@@ -0,0 +1,20 @@
+/**
+ * Local dependencies
+ */
+const Algorithm = require('./Algorithm')
+
+/**
+ * EcKeyGenParams
+ */
+class EcKeyGenParams extends Algorithm {
+
+  /**
+   * Constructor
+   */
+  constructor () {}
+}
+
+/**
+ * Export
+ */
+module.exports = EcKeyGenParams
