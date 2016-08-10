d8223e0846677c5064657b41160d36aea8aaeda7
Christian Smith <smith@anvil.io>
2016-08-09 20:52:22 -0700

    feat(RsaOaepParams): class definition

---
+++ src/algorithms/RsaOaepParams.js
@@ -0,0 +1,20 @@
+/**
+ * Local dependencies
+ */
+const Algorithm = require('./Algorithm')
+
+/**
+ * RsaOaepParams
+ */
+class RsaOaepParams extends Algorithm {
+
+  /**
+   * Constructor
+   */
+  constructor () {}
+}
+
+/**
+ * Export
+ */
+module.exports = RsaOaepParams
