434bdaa6ad5d384e545d5c1093c482b99450dd05
Christian Smith <smith@anvil.io>
2016-08-09 21:11:38 -0700

    wip(EcKeyImportParams): class definition

---
+++ src/algorithms/EcKeyImportParams.js
@@ -0,0 +1,20 @@
+/**
+ * Local dependencies
+ */
+const Algorithm = require('./Algorithm')
+
+/**
+ * EcKeyImportParams
+ */
+class EcKeyImportParams extends Algorithm {
+
+  /**
+   * Constructor
+   */
+  constructor () {}
+}
+
+/**
+ * Export
+ */
+module.exports = EcKeyImportParams
