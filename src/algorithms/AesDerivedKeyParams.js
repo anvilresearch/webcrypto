62e34dc53d0c671b9454da2a803e303b2a5b4ec2
Christian Smith <smith@anvil.io>
2016-08-09 21:19:10 -0700

    wip(AesDerivedKeyParams): class definition

---
+++ src/algorithms/AesDerivedKeyParams.js
@@ -0,0 +1,20 @@
+/**
+ * Local dependencies
+ */
+const Algorithm = require('./Algorithm')
+
+/**
+ * AesDerivedKeyParams
+ */
+class AesDerivedKeyParams extends Algorithm {
+
+  /**
+   * Constructor
+   */
+  constructor () {}
+}
+
+/**
+ * Export
+ */
+module.exports = AesDerivedKeyParams
