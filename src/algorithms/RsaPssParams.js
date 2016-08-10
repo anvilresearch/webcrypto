03c81cdaa5dba27669f7b93f45adeb77aa97912b
Christian Smith <smith@anvil.io>
2016-08-09 20:53:12 -0700

    feat(RsaPssParams): class definition

---
+++ src/algorithms/RsaPssParams.js
@@ -0,0 +1,20 @@
+/**
+ * Local dependencies
+ */
+const Algorithm = require('./Algorithm')
+
+/**
+ * RsaPssParams
+ */
+class RsaPssParams extends Algorithm {
+
+  /**
+   * Constructor
+   */
+  constructor () {}
+}
+
+/**
+ * Export
+ */
+module.exports = RsaPssParams
