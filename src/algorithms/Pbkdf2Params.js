cc290cda1c20ff0ac21e6ae0e48b0e2dbeaf30bb
Christian Smith <smith@anvil.io>
2016-08-09 21:53:04 -0700

    wip(Pbkdf2Params): class definition

---
+++ src/algorithms/Pbkdf2Params.js
@@ -0,0 +1,20 @@
+/**
+ * Local dependencies
+ */
+const Algorithm = require('./Algorithm')
+
+/**
+ * Pbkdf2Params
+ */
+class Pbkdf2Params extends Algorithm {
+
+  /**
+   * Constructor
+   */
+  constructor () {}
+}
+
+/**
+ * Export
+ */
+module.exports = Pbkdf2Params
