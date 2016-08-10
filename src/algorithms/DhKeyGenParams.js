f5d50d77ce2c76da491b0c681c16a280c104e6b7
Christian Smith <smith@anvil.io>
2016-08-09 21:45:57 -0700

    wip(DhKeyGenParams): class definition

---
+++ src/algorithms/DhKeyGenParams.js
@@ -0,0 +1,20 @@
+/**
+ * Local dependencies
+ */
+const Algorithm = require('./Algorithm')
+
+/**
+ * DhKeyGenParams
+ */
+class DhKeyGenParams extends Algorithm {
+
+  /**
+   * Constructor
+   */
+  constructor () {}
+}
+
+/**
+ * Export
+ */
+module.exports = DhKeyGenParams
