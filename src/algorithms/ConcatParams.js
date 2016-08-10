5f85a3d613dc7efaffd206da8fcee2b237e79ab9
Christian Smith <smith@anvil.io>
2016-08-09 21:48:56 -0700

    wip(ConcatParams): class definition

---
+++ src/algorithms/ConcatParams.js
@@ -0,0 +1,20 @@
+/**
+ * Local dependencies
+ */
+const Algorithm = require('./Algorithm')
+
+/**
+ * ConcatParams
+ */
+class ConcatParams extends Algorithm {
+
+  /**
+   * Constructor
+   */
+  constructor () {}
+}
+
+/**
+ * Export
+ */
+module.exports = ConcatParams
