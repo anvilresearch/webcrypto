793f9f7bbb08277712a384e4256192155f5c9b6a
Christian Smith <smith@anvil.io>
2016-08-09 21:37:02 -0700

    wip(HmacImportParams): class definition

---
+++ src/algorithms/HmacImportParams.js
@@ -0,0 +1,20 @@
+/**
+ * Local dependencies
+ */
+const Algorithm = require('./Algorithm')
+
+/**
+ * HmacImportParams
+ */
+class HmacImportParams extends Algorithm {
+
+  /**
+   * Constructor
+   */
+  constructor () {}
+}
+
+/**
+ * Export
+ */
+module.exports = HmacImportParams
