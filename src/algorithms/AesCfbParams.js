43fc7d87e2d1f12043b480b9efa5b28c16d60e3b
Christian Smith <smith@anvil.io>
2016-08-09 21:30:06 -0700

    wip(AesCfbParams): class definition

---
+++ src/algorithms/AesCfbParams.js
@@ -0,0 +1,21 @@
+
+/**
+ * Local dependencies
+ */
+const Algorithm = require('./Algorithm')
+
+/**
+ * AesCfbParams
+ */
+class AesCfbParams extends Algorithm {
+
+  /**
+   * Constructor
+   */
+  constructor () {}
+}
+
+/**
+ * Export
+ */
+module.exports = AesCfbParams
