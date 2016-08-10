26c979f11d44a88f055b3ef2c896aa853059924c
Christian Smith <smith@anvil.io>
2016-08-09 21:50:44 -0700

    wip(HkdfCtrParams): class definition

---
+++ src/algorithms/HkdfCtrParams.js
@@ -0,0 +1,20 @@
+/**
+ * Local dependencies
+ */
+const Algorithm = require('./Algorithm')
+
+/**
+ * HkdfCtrParams
+ */
+class HkdfCtrParams extends Algorithm {
+
+  /**
+   * Constructor
+   */
+  constructor () {}
+}
+
+/**
+ * Export
+ */
+module.exports = HkdfCtrParams
