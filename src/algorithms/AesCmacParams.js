d33de138a7b225f98eb7600a313b5e0d2b75618b
Christian Smith <smith@anvil.io>
2016-08-09 21:25:02 -0700

    wip(AesCmacParams): class definition

---
+++ src/algorithms/AesCmacParams.js
@@ -0,0 +1,20 @@
+/**
+ * Local dependencies
+ */
+const Algorithm = require('./Algorithm')
+
+/**
+ * AesCmacParams
+ */
+class AesCmacParams extends Algorithm {
+
+  /**
+   * Constructor
+   */
+  constructor () {}
+}
+
+/**
+ * Export
+ */
+module.exports = AesCmacParams
