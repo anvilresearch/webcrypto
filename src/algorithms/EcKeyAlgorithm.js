79e20c227699213bae5fccde1c8722f1d123abc0
Christian Smith <smith@anvil.io>
2016-08-09 21:10:50 -0700

    wip(EcKeyAlgorithm): class definition

---
+++ src/algorithms/EcKeyAlgorithm.js
@@ -0,0 +1,20 @@
+/**
+ * Local dependencies
+ */
+const KeyAlgorithm = require('./KeyAlgorithm')
+
+/**
+ * EcKeyAlgorithm
+ */
+class EcKeyAlgorithm extends KeyAlgorithm {
+
+  /**
+   * Constructor
+   */
+  constructor () {}
+}
+
+/**
+ * Export
+ */
+module.exports = EcKeyAlgorithm
