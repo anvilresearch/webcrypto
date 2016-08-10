921f787e704930312557d2f0e756bc973d31f166
Christian Smith <smith@anvil.io>
2016-08-09 21:20:04 -0700

    wip(AesKeyGenParams): class definition

---
+++ src/algorithms/AesKeyGenParams.js
@@ -0,0 +1,20 @@
+/**
+ * Local dependencies
+ */
+const Algorithm = require('./Algorithm')
+
+/**
+ * AesKeyGenParams
+ */
+class AesKeyGenParams extends Algorithm {
+
+  /**
+   * Constructor
+   */
+  constructor () {}
+}
+
+/**
+ * Export
+ */
+module.exports = AesKeyGenParams
