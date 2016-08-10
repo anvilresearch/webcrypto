0de1544a6adf2d8dd4ab56a007f3e913398d0532
Christian Smith <smith@anvil.io>
2016-08-09 21:45:32 -0700

    wip(DhKeyDeriveParams): class definition

---
+++ src/algorithms/DhKeyDeriveParams.js
@@ -0,0 +1,20 @@
+/**
+ * Local dependencies
+ */
+const Algorithm = require('./Algorithm')
+
+/**
+ * DhKeyDeriveParams
+ */
+class DhKeyDeriveParams extends Algorithm {
+
+  /**
+   * Constructor
+   */
+  constructor () {}
+}
+
+/**
+ * Export
+ */
+module.exports = DhKeyDeriveParams
