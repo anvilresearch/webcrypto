50577b2b95149f3675ee3dace0df9ad18212abf4
Christian Smith <smith@anvil.io>
2016-08-09 21:45:06 -0700

    wip(DhImportKeyParams): class definition

---
+++ src/algorithms/DhImportKeyParams.js
@@ -0,0 +1,20 @@
+/**
+ * Local dependencies
+ */
+const Algorithm = require('./Algorithm')
+
+/**
+ * DhImportKeyParams
+ */
+class DhImportKeyParams extends Algorithm {
+
+  /**
+   * Constructor
+   */
+  constructor () {}
+}
+
+/**
+ * Export
+ */
+module.exports = DhImportKeyParams
