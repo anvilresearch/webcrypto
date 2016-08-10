fb75c2e5969062b6276c2198fc07f99b45102080
Christian Smith <smith@anvil.io>
2016-08-09 21:09:53 -0700

    wip(EcdsaParams): class definition

---
+++ src/algorithms/EcdsaParams.js
@@ -0,0 +1,20 @@
+/**
+ * Local dependencies
+ */
+const Algorithm = require('./Algorithm')
+
+/**
+ * EcdsaParams
+ */
+class EcdsaParams extends Algorithm {
+
+  /**
+   * Constructor
+   */
+  constructor () {}
+}
+
+/**
+ * Export
+ */
+module.exports = EcdsaParams
