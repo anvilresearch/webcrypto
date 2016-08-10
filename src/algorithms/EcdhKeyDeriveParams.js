ec2d3281c8a0b5fc69488f2be558adda02004175
Christian Smith <smith@anvil.io>
2016-08-09 21:08:55 -0700

    wip(EcdhKeyDeriveParams): class definition

---
+++ src/algorithms/EcdhKeyDeriveParams.js
@@ -0,0 +1,20 @@
+/**
+ * Local dependencies
+ */
+const Algorithm = require('./Algorithm')
+
+/**
+ * EcdhKeyDeriveParams
+ */
+class EcdhKeyDeriveParams extends Algorithm {
+
+  /**
+   * Constructor
+   */
+  constructor () {}
+}
+
+/**
+ * Export
+ */
+module.exports = EcdhKeyDeriveParams
