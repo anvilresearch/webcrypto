1c634fa15b3c50a0d15b36c1eb9551e41cf64671
Christian Smith <smith@anvil.io>
2016-08-09 21:19:47 -0700

    wip(AesKeyAlgorithm): class definition

---
+++ src/algorithms/AesKeyAlgorithm.js
@@ -0,0 +1,20 @@
+/**
+ * Local dependencies
+ */
+const KeyAlgorithm = require('./KeyAlgorithm')
+
+/**
+ * AesKeyAlgorithm
+ */
+class AesKeyAlgorithm extends KeyAlgorithm {
+
+  /**
+   * Constructor
+   */
+  constructor () {}
+}
+
+/**
+ * Export
+ */
+module.exports = AesKeyAlgorithm
