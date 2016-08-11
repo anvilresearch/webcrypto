9c643c9180f0fbe1d2a36438f95321d1f303be97
Christian Smith <smith@anvil.io>
2016-08-10 23:28:16 -0700

    test(Algorithm): constructor tests

---
+++ test/algorithms/AlgorithmSpec.js
@@ -0,0 +1,41 @@
+/**
+ * Test dependencies
+ */
+const chai = require('chai')
+
+/**
+ * Assertions
+ */
+chai.should()
+const expect = chai.expect
+
+/**
+ * Code under test
+ */
+const Algorithm = require('../../src/algorithms/Algorithm')
+
+/**
+ * Tests
+ */
+describe('Algorithm', () => {
+  describe('constructor', () => {
+    describe('with string "algorithm" argument', () => {
+      it('should set instance name to argument value', () => {
+        let algorithm = new Algorithm('RSASSA-PKCS1-v1_5')
+        algorithm.name.should.equal('RSASSA-PKCS1-v1_5')
+      })
+    })
+    describe('with object "algorithm" argument', () => {
+      it('should assign argument property values to instance', () => {
+        let algorithm = new Algorithm({ name: 'RSASSA-PKCS1-v1_5' })
+        algorithm.name.should.equal('RSASSA-PKCS1-v1_5')
+      })
+
+      it('should require algorithm name to be a string', () => {
+        expect(() => {
+          new Algorithm({ other: false })
+        }).to.throw('Algorithm name must be a string')
+      })
+    })
+  })
+})
