d83edc15d5c1f33d2cbc5001207bf32f42d5949f
Christian Smith <smith@anvil.io>
2016-08-17 19:38:15 -0700

    refactor(RsaHashedKeyGenParams): move validation to method

--- src/algorithms/RsaHashedKeyGenParams.js
+++ src/algorithms/RsaHashedKeyGenParams.js
@@ -8,19 +8,23 @@ const RsaKeyGenParams = require('./RsaKeyGenParams')
  * RsaHashedKeyGenParams
  */
 class RsaHashedKeyGenParams extends RsaKeyGenParams {
+
+  /**
+   * constructor
+   */
   constructor (algorithm) {
     super(algorithm)
+  }
 
+  /**
+   * validate
+   */
+  validate () {
     // validate hash is an object
     if (typeof this.hash !== 'object') {
       throw new Error(
         'hash of RsaHashedKeyGenParams must be an object'
       )
-    }
-
-    // ensure hash is a KeyAlgorithm
-    if (!(this.hash instanceof KeyAlgorithm)) {
-      this.hash = new KeyAlgorithm(this.hash)
     }
   }
 }
