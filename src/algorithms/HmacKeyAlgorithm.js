b4c7497ada1a1b70cb462d410ac9e541d4f226d7
Christian Smith <smith@anvil.io>
2016-08-25 00:06:38 -0700

    wip(HmacKeyAlgorithm): method signatures

--- src/algorithms/HmacKeyAlgorithm.js
+++ src/algorithms/HmacKeyAlgorithm.js
@@ -12,6 +12,74 @@ class HmacKeyAlgorithm extends KeyAlgorithm {
    * Constructor
    */
   constructor () {}
+
+  /**
+   * sign
+   *
+   * @description
+   * Create a MAC
+   *
+   * @param {CryptoKey} key
+   * @param {BufferSource} data
+   *
+   * @return {string}
+   */
+  sign (key, data) {}
+
+  /**
+   * verify
+   *
+   * @description
+   * Verify a MAC
+   *
+   * @param {CryptoKey} key
+   * @param {BufferSource} signature
+   * @param {BufferSource} data
+   *
+   * @returns {Boolean}
+   */
+  verify (key, signature, data) {}
+
+  /**
+   * generateKey
+   *
+   * @description
+   * Generate HMAC key
+   *
+   * @param {HmacKeyGenParams} params
+   * @param {Boolean} extractable
+   * @param {Array} usages
+   *
+   * @returns {CryptoKey}
+   */
+  generateKey (params, extractable, usages) {}
+
+  /**
+   * importKey
+   *
+   * @description
+   *
+   * @param {string} format
+   * @param {string|JsonWebKey} keyData
+   * @param {KeyAlgorithm} algorithm
+   * @param {Boolean} extractable
+   * @param {Array} keyUsages
+   *
+   * @returns {CryptoKey}
+   */
+  importKey (format, keyData, algorithm, extractable, keyUsages) {}
+
+  /**
+   * exportKey
+   *
+   * @description
+   *
+   * @param {string} format
+   * @param {CryptoKey} key
+   *
+   * @returns {*}
+   */
+  exportKey (format, key) {}
 }
 
 /**
