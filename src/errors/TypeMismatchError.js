bbf757dfd3d6cf175c5c98f4350d4890ee9f0a38
Christian Smith <smith@anvil.io>
2016-09-28 08:10:32 -0700

    feat(TypeMismatchError): class definition

---
+++ src/errors/TypeMismatchError.js
@@ -0,0 +1,13 @@
+/**
+ * TypeMismatchError
+ */
+class TypeMismatchError extends Error {
+  constructor (message) {
+    super(message)
+  }
+}
+
+/**
+ * Export
+ */
+module.exports = TypeMismatchError
