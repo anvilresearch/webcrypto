36dd4845200577a401596ad4a287c6198ad56099
Christian Smith <smith@anvil.io>
2016-09-28 08:12:34 -0700

    feat(Crypto): add getRandomValues method

--- src/Crypto.js
+++ src/Crypto.js
@@ -1,7 +1,35 @@
 /**
  * Module dependencies
  */
+const {buf2ab} = require('./encodings')
+const legacyCrypto = require('crypto')
 const SubtleCrypto = require('./SubtleCrypto')
+const TypeMismatchError = require('./errors/TypeMismatchError')
+const QuotaExceededError = require('./errors/QuotaExceededError')
+
+/**
+ * integerTypes
+ */
+const integerTypes = [
+  Int8Array,
+  Uint8Array,
+  Int16Array,
+  Uint16Array,
+  Int32Array,
+  Uint32Array
+]
+
+/**
+ * integerGetByConstructor
+ */
+const integerGetByConstructor = {
+  'Int8Array': 'getInt8',
+  'Uint8Array': 'getUint8',
+  'Int16Array': 'getInt16',
+  'Uint16Array': 'getUint16',
+  'Int32Array': 'getInt32',
+  'Uint32Array': 'getUint32'
+}
 
 /**
  * Crypto interface
@@ -11,8 +39,31 @@ class Crypto {
   /**
    * getRandomValues
    */
-  getRandomValues () {
+  getRandomValues (typedArray) {
+    if (!integerTypes.some(type => typedArray instanceof type)) {
+      throw new TypeMismatchError()
+    }
 
+    let byteLength = typedArray.byteLength
+
+    if (byteLength > 65536) {
+      throw new QuotaExceededError()
+    }
+
+    let type = typedArray.constructor
+    let method = integerGetByConstructor[type.name]
+    let totalBytes = byteLength * typedArray.length
+    let buffer = legacyCrypto.randomBytes(totalBytes)
+    let arrayBuffer = buf2ab(buffer, type)
+    let dataView = new DataView(arrayBuffer)
+
+    for (let byteIndex = 0; byteIndex < totalBytes; byteIndex += byteLength) {
+      let integer = dataView[method](byteIndex)
+      let arrayIndex = byteIndex / byteLength
+      typedArray[arrayIndex] = integer
+    }
+
+    return typedArray
   }
 
   /**
