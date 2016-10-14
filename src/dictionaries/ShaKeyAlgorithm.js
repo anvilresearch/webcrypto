fd686a37a5db7b1a855b3cbf53c9234f44e05bcf
Christian Smith <smith@anvil.io>
2016-10-14 11:54:37 -0700

    test(ShaKeyAlgorithm): getters and digest method

--- src/dictionaries/ShaKeyAlgorithm.js
+++ src/dictionaries/ShaKeyAlgorithm.js
@@ -41,34 +41,34 @@ class ShaKeyAlgorithm extends KeyAlgorithm {
   digest (algorithm, data) {
     let result
     let {name} = algorithm
-    let ab = data.buffer
-    let dataBuffer = new Buffer(new Uint8Array(ab))
+
+    data = Buffer.from(data)
 
     if (name === 'SHA-1') {
       let hash = crypto.createHash('sha1')
-      hash.update(dataBuffer)
+      hash.update(data)
       result = hash.digest()
 
     } else if (name === 'SHA-256') {
       let hash = crypto.createHash('sha256')
-      hash.update(dataBuffer)
+      hash.update(data)
       result = hash.digest()
 
     } else if (name === 'SHA-384') {
       let hash = crypto.createHash('sha384')
-      hash.update(dataBuffer)
+      hash.update(data)
       result = hash.digest()
 
     } else if (name === 'SHA-512') {
       let hash = crypto.createHash('sha512')
-      hash.update(dataBuffer)
+      hash.update(data)
       result = hash.digest()
 
     } else {
-      throw new OperationError()
+      throw new OperationError(`${name} is not a supported algorithm`)
     }
 
-    return Uint8Array.from(result)
+    return Uint8Array.from(result).buffer
   }
 }
 
