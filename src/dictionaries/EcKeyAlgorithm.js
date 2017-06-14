131ef14280aada4d81a53a749f6f8441fc75a429
Greg Linklater <greglink49@gmail.com>
2017-06-14 12:32:56 +0200

    refactor(ECDSA): rename to conform to WC interface

--- src/dictionaries/EcKeyAlgorithm.js
+++ src/dictionaries/EcKeyAlgorithm.js
@@ -4,6 +4,16 @@
 const KeyAlgorithm = require('./KeyAlgorithm')
 
 /**
+ * Mapping
+ */
+const mapping = [
+  { namedCurve: 'P-256', name: 'secp256r1', alg: 'ES256' },
+  { namedCurve: 'P-384', name: 'secp384r1', alg: 'ES384' },
+  { namedCurve: 'P-512', name: 'secp521r1', alg: 'ES512' },
+  { namedCurve: 'K-256', name: 'secp256k1', alg: 'KS256' },
+]
+
+/**
  * EcKeyAlgorithm
  */
 class EcKeyAlgorithm extends KeyAlgorithm {
@@ -11,7 +21,13 @@ class EcKeyAlgorithm extends KeyAlgorithm {
   /**
    * Constructor
    */
-  constructor () {}
+  constructor (algorithm) {
+    super(algorithm)
+  }
+
+  static get mapping () {
+    return mapping
+  }
 }
 
 /**
