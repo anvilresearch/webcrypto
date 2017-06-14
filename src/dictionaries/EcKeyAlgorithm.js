acede34d150f41187d262e90f3bb129bc9573f1a
Greg Linklater <greglink49@gmail.com>
2017-06-14 20:39:57 +0200

    fix(EcKeyAlgorithm): correct curve name for nist P-256

--- src/dictionaries/EcKeyAlgorithm.js
+++ src/dictionaries/EcKeyAlgorithm.js
@@ -7,7 +7,7 @@ const KeyAlgorithm = require('./KeyAlgorithm')
  * Mapping
  */
 const mapping = [
-  { namedCurve: 'P-256', name: 'secp256r1', alg: 'ES256' },
+  { namedCurve: 'P-256', name: 'prime256v1', alg: 'ES256' },
   { namedCurve: 'P-384', name: 'secp384r1', alg: 'ES384' },
   { namedCurve: 'P-512', name: 'secp521r1', alg: 'ES512' },
   { namedCurve: 'K-256', name: 'secp256k1', alg: 'KS256' },
