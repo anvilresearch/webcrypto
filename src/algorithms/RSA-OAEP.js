a168b18ec7669f45e698e722d814be82554ef8ee
JC <jcbailey1991@gmail.com>
2017-08-31 20:22:29 +0200

    wip(rsa-oaep): finished, lacking x-platform support

--- src/algorithms/RSA-OAEP.js
+++ src/algorithms/RSA-OAEP.js
@@ -86,27 +86,30 @@ class RSA_OAEP extends Algorithm {
      * @returns {Array}
      */
     encrypt (algorithm, key, data) {
-      // // 1. Ensure correct iv length
-      // if (algorithm.iv.byteLength !== 16) {
-      //   throw new OperationError('IV Length must be exactly 16 bytes')
-      // }
-      
-      // // 2. Add padding to erronuous length text as described here:
-      // // https://tools.ietf.org/html/rfc2315#section-10.3
-      // let paddedPlaintext = data
+    let result
+      // 1. Ensure the key is a public type only
+      if (key.type !== 'public') {
+        throw new InvalidAccessError('Encrypt requires a public key')
+      }
 
-      // // 3. Do the encryption
-      // let cipherName
-      // if (key.algorithm.name === 'RSA-OAEP' && [128,192,256].includes(key.algorithm.length)){
-      //   cipherName = 'AES-' + key.algorithm.length + '-CBC'
-      // } else {
-      //   throw new DataError('Invalid RSA-OAEP and length pair.')
-      // }
-      // let cipher = crypto.createCipheriv(cipherName,key.handle,Buffer.from(algorithm.iv))
-      // let ciphertext = cipher.update(Buffer.from(data))
+      // 2. Assign label
+      // TODO Investigate use for label within context
+      let label
+      if (algorithm.label !== undefined){
+        label = algorithm.label
+      } else {
+        label = ""
+      }
       
-      // // 4. Return result
-      // return Uint8Array.from(Buffer.concat([ciphertext,cipher.final()])).buffer
+      // 3-5. Attempt to encrypt using crypto lib
+      try {
+        data = Buffer.from(data)
+        result = crypto.publicEncrypt({key: key.handle, padding: crypto.constants.RSA_PKCS1_OAEP_PADDING},data)
+      } catch (error) {
+        throw new OperationError(error.message)
+      }
+      // 7. Return resulting buffer
+      return result.buffer
     }
 
     /**
@@ -122,26 +125,33 @@ class RSA_OAEP extends Algorithm {
      * @returns {Array}
      */
     decrypt (algorithm, key, data) {
-      // // 1. Ensure correct iv length
-      // if (algorithm.iv.byteLength !== 16){
-      //   throw new OperationError('IV Length must be exactly 16 bytes')
-      // }
+      let result
+      // 1. Ensure the key is a private type only
+      if (key.type !== 'private') {
+        throw new InvalidAccessError('Decrypt requires a private key')
+      }
+
+      // 2. Assign label
+      let label
+      if (algorithm.label !== undefined){
+        label = algorithm.label
+      } else {
+        label = ""
+      }
       
-      // // 2. Perform the decryption 
-      // let cipherName
-      // if (key.algorithm.name === 'RSA-OAEP' && [128,192,256].includes(key.algorithm.length)){
-      //   cipherName = 'AES-' + key.algorithm.length + '-CBC'
-      // } else {
-      //   throw new DataError('Invalid RSA-OAEP and length pair.')
-      // }
-      // let decipher = crypto.createDecipheriv(cipherName,key.handle,Buffer.from(algorithm.iv))
-      // let ciphertext = decipher.update(Buffer.from(data))
-      // let plaintext = Array.from(Buffer.concat([ciphertext,decipher.final()]))
-
-      // // 3-5. Text de-padding performed by crypto.decipher
-
-      // // 6. Return resulting ArrayBuffer
-      // return Uint8Array.from(plaintext).buffer
+      // 3-5. Attempt to decrypt using crypto lib
+      try {
+        data = Buffer.from(data)
+        result = crypto.privateDecrypt({
+          key: key.handle, 
+          padding: crypto.constants.RSA_PKCS1_OAEP_PADDING
+        },
+          data)
+      } catch (error) {
+        throw new OperationError(error.message)
+      }
+      // 7. Return resulting buffer
+      return result.buffer
     }
 
     /**
@@ -371,7 +381,7 @@ class RSA_OAEP extends Algorithm {
    * @returns {*}
    */
   exportKey (format, key) {
-      // 1. Setup resulting var
+    // 1. Setup resulting var
     let result
 
     // 2. Validate handle slot
@@ -381,11 +391,11 @@ class RSA_OAEP extends Algorithm {
 
     // 3.1. "spki" format
     if (format === 'spki') {
-      throw new CurrentlyNotSupportedError(format,"jwk' or 'raw")
+      throw new CurrentlyNotSupportedError(format,"jwk")
     }
     // 3.2. "pkcs8" format
     else if (format === 'pkcs8') {
-      throw new CurrentlyNotSupportedError(format,"jwk' or 'raw")
+      throw new CurrentlyNotSupportedError(format,"jwk")
     }
     // 2.3. "jwk" format
     else if (format === 'jwk') {
@@ -396,40 +406,42 @@ class RSA_OAEP extends Algorithm {
       jwk.kty = "RSA"
 
       // 2.3.3. Determine alg from hash
-      // TODO continue here
+      let hash = key.algorithm.hash.name
+
+      // 2.3.3.1. Hash is "SHA-1"
       if (hash === 'SHA-1') {
-        jwk.alg = 'RS1'
-      } else if (hash === 'SHA-256') {
-        jwk.alg = 'RS256'
-      } else if (hash === 'SHA-384') {
-        jwk.alg = 'RS384'
-      } else if (hash === 'SHA-512') {
-        jwk.alg = 'RS512'
-      } else {
+        jwk.alg = 'RSA-OAEP'
+      } 
+      // 2.3.3.2. Hash is "SHA-256"
+      else if (hash === 'SHA-256') {
+        jwk.alg = 'RSA-OAEP-256'
+      } 
+      // 2.3.3.3. Hash is "SHA-384"
+      else if (hash === 'SHA-384') {
+        jwk.alg = 'RSA-OAEP-384'
+      } 
+      // 2.3.3.4. Hash is "SHA-512"
+      else if (hash === 'SHA-512') {
+        jwk.alg = 'RSA-OAEP-512'
+      } 
+      // 2.3.3.5. Hash is other value
+      else {
         // TODO other applicable specifications
+        throw new CurrentlyNotSupportedError(format,"SHA-1")
       }
+      // 2.3.4-5. Assign corresponding field from JWA specification
+      Object.assign(jwk, keyto.from(key.handle, 'pem').toJwk(key.type))
 
-
-      // 2.3.4. Set "key_ops" field
+      // 2.3.6. Set "key_ops" field
       jwk.key_ops = key.usages
 
-      // 2.3.5. Set "ext" field
+      // 2.3.7. Set "ext" field
       jwk.ext = key.extractable
 
-      // 2.3.6. Set result to jwk object
+      // 2.3.8. Set result to jwk object
       result = jwk
     }
-    // 3.4. "raw" format
-    else if (format === 'raw') {
-      // 3.4.1. Validate that the internal use is public
-      if (key.type !== 'public'){
-        throw new InvalidAccessError('Can only access public key data.')
-      }
-      // 3.4.2. Omitted due to redundancy
-      // 3.4.3. Let resulting data be Buffer containing data
-      result = Buffer.from(key.handle)
-    }
-    // 3.5. Otherwise bad format
+    // 3.4. Otherwise bad format
     else {
       throw new KeyFormatNotSupportedError(format)
     }
@@ -444,6 +456,8 @@ class RSA_OAEP extends Algorithm {
  */
 module.exports = RSA_OAEP
 
+
+/*
 let rsa = new RSA_OAEP({name:"RSA-OAEP",hash:{name: "SHA-256"}})
 let kp = rsa.generateKey(
   {
@@ -457,15 +471,20 @@ let kp = rsa.generateKey(
   )
 // console.log("kp",kp)
 
-let imp = rsa.importKey(
+let imp_priv = rsa.importKey(
   "jwk", //can be "jwk" (public or private), "spki" (public only), or "pkcs8" (private only)
-    {   //this is an example jwk key, other key types are Uint8Array objects
-        kty: "RSA",
-        e: "AQAB",
-        n: "vGO3eU16ag9zRkJ4AK8ZUZrjbtp5xWK0LyFMNT8933evJoHeczexMUzSiXaLrEFSyQZortk81zJH3y41MBO_UFDO_X0crAquNrkjZDrf9Scc5-MdxlWU2Jl7Gc4Z18AC9aNibWVmXhgvHYkEoFdLCFG-2Sq-qIyW4KFkjan05IE",
-        alg: "RSA-OAEP-256",
-        ext: true,
+{"alg":"RSA-OAEP-256","d":"mp3ziE80B79bku3FTVEDmvB2thL3AiMnzcU-Fx83HuJqbzMSeMGie75GiDjLDH_Q8ty3VRXUTQdoOoTcDatkhkqxY34omugejZ9jmfpwBaGgygYbYMdScYj-Nq6NgdN5iidjieEeT1hcy8htVqWJTawcLhyZ-TCRIMCvPHzGlrL63B4jK7nZkDOYnugXUs342DpkkocrGwUhMLDnjn4pliMtHa8YAD22GjOwiSMQnZipLkULGlT-vNc5yReovMeZ8YEeminOSDsnvsr2G8mHbC-9Py8RKbsR0HVz7R4Mc11iiG4b22WaruMfYDmfNhT_xbnAVqhwuef2KaBkAL8IAQ","dp":"nDnoGbRIIhJNN-1Ahe6NjGtYQSp9pSO7W9Zj_49sX6McVXK11Y3dNBzi8ipZNS5qjfRBfYXpP-jqyUJWiOG336XWrjoLCmn7OGV8XlGO5VX_a8Rq1soWv00ZyW5BjqsDN8LuWfysyTjQ8kVbagAIkopk_KeO1Monv-w6RIIyYDs","dq":"jg5PYhX9Fhfrjzcvk2JEDPMWvYhYsKip5CaWj7bON4emqQr2DawH9E5sR_1LXI9soBji2x65C-iXodgUKswIVuGKgOTX3DH9fKsjduwsoE5Kfui2eHO6mQrhcTyCtHu2vOiRV58qln5y_RnCaRTzNBqpfVpOMUBFr5XAx1QRlgE","e":"AQAB","ext":true,"key_ops":["decrypt"],"kty":"RSA","n":"q5vBmLlVYveWFgeqhKhbwWEEbuQRJ-VEb1JbEEFEWm1LPF-YaqKlSngVq7c_dZcJRatYw2U2gBpQIKjMkzrMXQSlbuG0Skumv8qpb3_aQ7WqhUjTC4-1VG9VWuROUTu9Zalra7aBlIJ2kRHVDsDFgKpFIlcXTD7kOV_mdeg4aSOrV-7LH4vQOPbb42Y3boBjTPX4RuqimuwAt91zO8czRu0veVJRnWLynQ82dil3SRla88rIkXFYm9pVCWBgsczcHOyqAdhPVD8tP62aisks9T3vs1OMDo680j454yO3hOsx6szdiVf1Q9YDMTxzkTZtG3ys6U1yNd4sAwRJ3-hK_w","p":"1kpspcHYpxjUnhMF-ETQvgVfUPVC9PxoTqozkK3VrW4v6gxqf0A9vBqXkqOvAYOpKNOz0ZizrJ6h--xYgpCuStxHq0L_dhcqgc6HPuLweNEottXqiBZnmVx9hPhDUqk0gdi8trlenc0_-Zqq8vCI1fNBkk2c_ydCzzGfGV9krH8","q":"zQKUJl0fhixAK316nS16dbOUTxjclL3VLxAMNkiNJeSdfKNXM3I-MTz0G5BbEFGW35U5XW4KYy4iBI2ka_db2-MOBEfYf_Q9JUb2eS1Gq-5KzVmpu9LrQlkJsl_bHYIDhWtNJD1BffFObFpIq1MSkFjrjEykUBX08zvqPwePIYE","qi":"OuvFJGnC_4GonCVZl9cP89p3KhPIbqzCuOcqIp_7Bz3ElE7ZfFvgy1XYmyo5tr0XKowGH9eEh9Ymd6lfxP5gqxgVvfuMjCJhdBLAIKFI9ggyzbaCl0PNvSpcZYNLYDjPuxOgSKjxrd9HgX-zSKNah0nGu4OxEYlhzl7KWX5bV6c"},
+    {   //these are the algorithm options
+        name: "RSA-OAEP",
+        hash: {name: "SHA-256"}, //can be "SHA-1", "SHA-256", "SHA-384", or "SHA-512"
     },
+    true, //whether the key is extractable (i.e. can be used in exportKey)
+    ["decrypt"]
+  )
+
+let imp_pub = rsa.importKey(
+  "jwk", //can be "jwk" (public or private), "spki" (public only), or "pkcs8" (private only)
+{"alg":"RSA-OAEP-256","e":"AQAB","ext":true,"key_ops":["encrypt"],"kty":"RSA","n":"q5vBmLlVYveWFgeqhKhbwWEEbuQRJ-VEb1JbEEFEWm1LPF-YaqKlSngVq7c_dZcJRatYw2U2gBpQIKjMkzrMXQSlbuG0Skumv8qpb3_aQ7WqhUjTC4-1VG9VWuROUTu9Zalra7aBlIJ2kRHVDsDFgKpFIlcXTD7kOV_mdeg4aSOrV-7LH4vQOPbb42Y3boBjTPX4RuqimuwAt91zO8czRu0veVJRnWLynQ82dil3SRla88rIkXFYm9pVCWBgsczcHOyqAdhPVD8tP62aisks9T3vs1OMDo680j454yO3hOsx6szdiVf1Q9YDMTxzkTZtG3ys6U1yNd4sAwRJ3-hK_w"},
     {   //these are the algorithm options
         name: "RSA-OAEP",
         hash: {name: "SHA-256"}, //can be "SHA-1", "SHA-256", "SHA-384", or "SHA-512"
@@ -473,4 +492,41 @@ let imp = rsa.importKey(
     true, //whether the key is extractable (i.e. can be used in exportKey)
     ["encrypt"]
   )
-console.log("imp",imp)
+
+
+// console.log("imp",imp)
+
+// let exp = rsa.exportKey(
+//   "jwk", //can be "jwk" (public or private), "spki" (public only), or "pkcs8" (private only)
+//   imp //can be a publicKey or privateKey, as long as extractable was true
+// )
+// console.log("exp",exp)
+
+// console.log(rsa.exportKey("jwk",kp.privateKey))
+
+
+let enc = rsa.encrypt(
+  {
+        name: "RSA-OAEP",
+        //label: Uint8Array([...]) //optional
+  },
+  imp_pub, //from generateKey or importKey above
+  new TextEncoder().encode("helloworld")
+)
+console.log(JSON.stringify(Array.from(new Uint8Array(enc))))
+
+// console.log(imp.handle)
+
+// let webenc = new Uint8Array([76,104,42,180,203,222,7,52,186,119,142,78,16,83,159,204,66,26,79,138,1,178,37,190,6,212,9,43,216,235,85,30,47,24,187,88,110,26,15,93,83,135,50,71,142,252,121,193,59,146,48,41,236,88,30,55,113,206,116,10,229,87,131,123,233,118,234,52,164,225,253,154,91,184,210,203,35,50,246,55,88,184,83,20,31,238,174,70,178,5,104,143,249,212,57,178,68,79,92,164,107,203,82,1,3,149,215,241,227,34,82,116,0,77,177,182,79,236,215,230,205,246,27,166,90,159,132,219,82,212,108,31,255,203,9,142,227,30,241,252,233,136,168,166,210,7,122,40,78,21,86,203,15,52,125,156,82,93,204,140,124,63,57,249,225,100,64,199,177,3,171,240,71,82,87,208,18,127,81,22,73,27,168,177,119,93,73,76,102,60,242,247,76,122,104,79,102,154,133,193,143,119,61,125,84,245,138,161,115,162,42,175,227,199,74,185,231,92,45,246,170,211,172,161,190,193,239,49,131,133,243,72,8,85,55,6,171,184,145,8,42,138,250,133,219,243,201,78,72,215,176,149,241,42,80,115])
+// // console.log("webenc",webenc)
+let dec = rsa.decrypt(
+   {
+        name: "RSA-OAEP",
+        //label: Uint8Array([...]) //optional
+    },
+    imp_priv, //from generateKey or importKey above
+    enc //ArrayBuffer of the data
+)
+// console.log("dec",new TextDecoder().decode(dec))
+
+*/
